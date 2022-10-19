f06a1803cc6afaa026b1981070bcbd4855f1aa1c
dpirohov <1355579555AsdF>
2022-10-19 16:35:32 +0300

    Changed protocol, removed time and status code from protocol

--- utils/protocol/message.py
+++ utils/protocol/message.py
@@ -4,13 +4,10 @@ import dataclasses
 @dataclasses.dataclass
 class Message:
     command: str
-    status_code: str
     sender: str
     receiver: str
     token: str
     content: str
-    time: str
 
     def form_protocol(self):
-        return f'{self.command};;{self.status_code};;{self.sender};;{self.receiver};;' \
-               f'{self.token};;{self.content};;{self.time}'
+        return f'{self.command};;{self.sender};;{self.receiver};;{self.token};;{self.content}'
