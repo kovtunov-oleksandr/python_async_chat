f4d3ba03c99bccdc44ed7f2835101e0878841f01
dpirohov <1355579555AsdF>
2022-10-19 20:42:27 +0300

    Delete redundant logging, add get_handler method

--- utils/protocol/server.py
+++ utils/protocol/server.py
@@ -11,27 +11,27 @@ class Server:
         self.host = host
         self.port = port
         self.data_coding_format = data_coding_format
-        self.sessions = {}
-        self.command_handler_map = {}
+        self.__command_handler_map = {}
 
     async def run_server(self):
-        server = await asyncio.start_server(self.handle_connection, self.host, self.port)
+        server = await asyncio.start_server(self._handle_connection, self.host, self.port)
         logger.info(f'Server started on {self.host}:{self.port}')
-        logger.info(f'Command handle map is formed: {self.command_handler_map}')
         async with server:
             await server.serve_forever()
 
-    async def handle_connection(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
+    async def _handle_connection(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
         while True:
             data = await reader.read(1024)
-            logger.debug(f'Received from {writer.get_extra_info("peername")} data: {data}')
+            logger.debug(f'IN: {writer.get_extra_info("peername")} data: {data}')
             connection = Connection(writer, reader)
             message: Message = parse_protocol_message(data.decode(self.data_coding_format))
             logger.debug(f'Data decoded: {message.form_protocol()}')
-            await self.command_handler_map.get(message.command)(message)
+            await self._get_handler(message, connection)
 
-    def add_handler(self, method: str):
-        def inner(func):
-            logger.info(f'Handler collector has started, new handler found: {func.__name__}')
-            self.command_handler_map[method] = func
-        return inner
+    async def _get_handler(self, message: Message, connection: Connection):
+        await self.__command_handler_map[message.command](message, connection)
+
+    def message_handler(self, method: str):
+        def decorator(func):
+            self.__command_handler_map[method] = func
+        return decorator
