ebf3757113188be4fe358041424674b8e433ffd8
dpirohov <1355579555AsdF>
2022-10-19 20:40:28 +0300

    Add connection argument

--- server/routes/sign_in.py
+++ server/routes/sign_in.py
@@ -1,7 +1,8 @@
 from server.config import server
 from utils.protocol.message import Message
+from utils.protocol.connection import Connection
 
 
-@server.add_handler('sign_in')
-async def sign_in(request: Message):
+@server.message_handler('sign_in')
+async def sign_in(request: Message, connection: Connection):
     print('SIGN IN METHOD')
--- server/routes/sign_up.py
+++ server/routes/sign_up.py
@@ -1,7 +1,8 @@
 from server.config import server
 from utils.protocol.message import Message
+from utils.protocol.connection import Connection
 
 
-@server.add_handler('sign_up')
-async def sign_up(request: Message):
+@server.message_handler('sign_up')
+async def sign_up(request: Message, connection: Connection):
     print('SIGN UP METHOD')
