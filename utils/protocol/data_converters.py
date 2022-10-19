c1e54b7b5575fd00dba95e05352a83131b00c525
dpirohov <1355579555AsdF>
2022-10-19 16:26:11 +0300

    Deleted redundant time function

--- utils/protocol/data_converters.py
+++ utils/protocol/data_converters.py
@@ -1,9 +1,4 @@
-import datetime
 from utils.protocol.message import Message
-
-
-def form_timestamp():
-    return datetime.datetime.now().strftime('%H:%M:%S')
 
 
 def parse_protocol_message(data: str) -> Message:
