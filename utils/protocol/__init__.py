3b6bad4403d3d5b2ae08985b55b8e21cc31e7bea
dpirohov <1355579555AsdF>
2022-10-19 16:33:17 +0300

    Refactored imports in __init__

--- utils/protocol/__init__.py
+++ utils/protocol/__init__.py
@@ -1,4 +1,3 @@
 from utils.protocol.connection import Connection
 from utils.protocol.message import Message
 from utils.protocol.server import Server
-from utils.protocol.data_converters import form_timestamp
