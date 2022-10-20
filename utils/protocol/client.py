e52a8d234ac468860535066d940ea0330fb8a445
Roman Poberezhnyi <102520613+Alaru-dev@users.noreply.github.com>
2022-10-20 20:20:36 +0000

    Update client.py
    add connection=None to init

--- utils/protocol/client.py
+++ utils/protocol/client.py
@@ -7,6 +7,7 @@ class Client:
     def __init__(self, host, port):
         self.host = host
         self.port = port
+        self.connection = None
 
     async def connect(self):
         reader, writer = await asyncio.open_connection(self.host, self.port)
