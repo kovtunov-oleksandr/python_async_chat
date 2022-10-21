809792bd0ead8fb3ad0a83b3e3b15d6fe9a3aebc
KateSamus <109095866+KateSamus@users.noreply.github.com>
2022-10-21 17:41:58 +0300

    fix main func

--- desktop/main.py
+++ desktop/main.py
@@ -4,14 +4,14 @@ from desktop.windows.start_window import StartWindow
 
 
 async def main():
-    if __name__ == "__main__":
-        import sys
-        app = QtWidgets.QApplication(sys.argv)
-        client = None
-        window = StartWindow(client)
-        window.setup_ui()
-        window.show()
-        sys.exit(app.exec_())
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    client = None
+    start_window = StartWindow(client)
+    start_window.setup_ui()
+    start_window.window.show()
+    sys.exit(app.exec_())
 
 
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
