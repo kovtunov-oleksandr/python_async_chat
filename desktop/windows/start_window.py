90ea97abbdff18d73eee2fb760af3a83fcf1b0ed
KateSamus <109095866+KateSamus@users.noreply.github.com>
2022-10-21 17:45:55 +0300

    add initialization of QMainWindow as class variable except of inherit it

--- desktop/windows/start_window.py
+++ desktop/windows/start_window.py
@@ -1,45 +1,45 @@
 from PyQt5.QtCore import QRect
 from PyQt5.QtWidgets import QLabel, QPushButton
-from desktop.windows import BaseWindow
+from desktop.windows import BaseWindow, StartWindowMixin
 from desktop.windows.auth.sign_up_window import SignUpWindow
 from desktop.windows.auth.sign_in_window import SignInWindow
 
 
-class StartWindow(BaseWindow):
+class StartWindow(BaseWindow, StartWindowMixin):
     def __init__(self, client):
         super().__init__(client)
-        self.client = client
+        self.window = self.create_window()
 
     def open_sign_in_menu(self):
-        self.window = SignInWindow(self.client)
-        self.window.setup_ui()
-        self.window.show()
-        self.close()
+        sign_in_window = SignInWindow(self.client)
+        sign_in_window.setup_ui()
+        sign_in_window.window.show()
+        self.window.close()
 
     def open_sign_up_menu(self):
-        self.window = SignUpWindow(self.client)
-        self.window.setup_ui()
-        self.window.show()
-        self.close()
+        sign_up_window = SignUpWindow(self.client)
+        sign_up_window.setup_ui()
+        sign_up_window.window.show()
+        self.window.close()
 
     def setup_ui(self):
-        self.setWindowTitle("Welcome Menu")
-        self.resize(800, 600)
-        self.setStyleSheet(
+        self.window.setWindowTitle("Welcome Menu")
+        self.window.resize(800, 600)
+        self.window.setStyleSheet(
             "background-color: rgb(244, 244, 244);\n" "color: rgb(27, 28, 28);"
         )
 
-        self.title = QLabel(self)
-        self.title.setGeometry(QRect(170, 160, 450, 50))
-        self.title.setText("Welcome to BeetrootAcademy Chat!")
-        self.title.setStyleSheet(
+        self.window.title = QLabel(self.window)
+        self.window.title.setGeometry(QRect(170, 160, 450, 50))
+        self.window.title.setText("Welcome to BeetrootAcademy Chat!")
+        self.window.title.setStyleSheet(
             "\n" "color: rgb(0, 0, 0);\n" 'font: 18pt ".SF NS Text";'
         )
 
-        self.sign_in = QPushButton(self, clicked=self.open_sign_in_menu)
-        self.sign_in.setText("Sign In")
-        self.sign_in.setGeometry(QRect(310, 260, 181, 41))
-        self.sign_in.setStyleSheet(
+        self.window.sign_in = QPushButton(self.window, clicked=self.open_sign_in_menu)
+        self.window.sign_in.setText("Sign In")
+        self.window.sign_in.setGeometry(QRect(310, 260, 181, 41))
+        self.window.sign_in.setStyleSheet(
             "color: rgb(250, 255, 255);\n"
             "background-color: rgb(167, 10, 61);\n"
             "border-style: outset;\n"
@@ -47,10 +47,10 @@ class StartWindow(BaseWindow):
             'font: 14pt "Arial";'
         )
 
-        self.sign_up = QPushButton(self, clicked=self.open_sign_in_menu)
-        self.sign_up.setText("Sign Up")
-        self.sign_up.setGeometry(QRect(310, 310, 181, 41))
-        self.sign_up.setStyleSheet(
+        self.window.sign_up = QPushButton(self.window, clicked=self.open_sign_up_menu)
+        self.window.sign_up.setText("Sign Up")
+        self.window.sign_up.setGeometry(QRect(310, 310, 181, 41))
+        self.window.sign_up.setStyleSheet(
             "color: rgb(250, 255, 255);\n"
             "background-color: rgb(167, 10, 61);\n"
             "border-style:outset;\n"
