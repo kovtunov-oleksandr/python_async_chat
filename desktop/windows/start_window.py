e5f978164029a3425f00910da12054a9cdb9a49a
KateSamus <109095866+KateSamus@users.noreply.github.com>
2022-10-23 13:02:50 +0300

    Creat separate funcs to set up each window object Brought the settings of window and title to BaseWindow Created general setup_start_window func Format file by black and flake8

--- desktop/windows/start_window.py
+++ desktop/windows/start_window.py
@@ -1,41 +1,23 @@
 from PyQt5.QtCore import QRect
-from PyQt5.QtWidgets import QLabel, QPushButton
+from PyQt5.QtWidgets import QPushButton
 from desktop.windows import BaseWindow, StartWindowMixin
-from desktop.windows.auth.sign_up_window import SignUpWindow
-from desktop.windows.auth.sign_in_window import SignInWindow
+from desktop.windows.auth import *
 
 
 class StartWindow(BaseWindow, StartWindowMixin):
-    def __init__(self, client):
-        super().__init__(client)
-        self.window = self.create_window()
-
     def open_sign_in_menu(self):
         sign_in_window = SignInWindow(self.client)
-        sign_in_window.setup_ui()
+        sign_in_window.setup_sign_in_window()
         sign_in_window.window.show()
         self.window.close()
 
     def open_sign_up_menu(self):
         sign_up_window = SignUpWindow(self.client)
-        sign_up_window.setup_ui()
+        sign_up_window.setup_sign_up_window()
         sign_up_window.window.show()
         self.window.close()
 
-    def setup_ui(self):
-        self.window.setWindowTitle("Welcome Menu")
-        self.window.resize(800, 600)
-        self.window.setStyleSheet(
-            "background-color: rgb(244, 244, 244);\n" "color: rgb(27, 28, 28);"
-        )
-
-        self.window.title = QLabel(self.window)
-        self.window.title.setGeometry(QRect(170, 160, 450, 50))
-        self.window.title.setText("Welcome to BeetrootAcademy Chat!")
-        self.window.title.setStyleSheet(
-            "\n" "color: rgb(0, 0, 0);\n" 'font: 18pt ".SF NS Text";'
-        )
-
+    def setup_sign_in_button(self):
         self.window.sign_in = QPushButton(self.window, clicked=self.open_sign_in_menu)
         self.window.sign_in.setText("Sign In")
         self.window.sign_in.setGeometry(QRect(310, 260, 181, 41))
@@ -47,6 +29,7 @@ class StartWindow(BaseWindow, StartWindowMixin):
             'font: 14pt "Arial";'
         )
 
+    def setup_sign_up_button(self):
         self.window.sign_up = QPushButton(self.window, clicked=self.open_sign_up_menu)
         self.window.sign_up.setText("Sign Up")
         self.window.sign_up.setGeometry(QRect(310, 310, 181, 41))
@@ -57,3 +40,9 @@ class StartWindow(BaseWindow, StartWindowMixin):
             "border-radius:10px;\n"
             'font: 14pt "Arial";'
         )
+
+    def setup_start_window(self):
+        self.setup_window("Start Window")
+        self.setup_title("Welcome to BeetrootAcademy Chat!", 160)
+        self.setup_sign_in_button()
+        self.setup_sign_up_button()
