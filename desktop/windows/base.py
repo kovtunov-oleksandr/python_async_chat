c5062ba586e5428f227eb92ac4593f6c14d601d3
KateSamus <109095866+KateSamus@users.noreply.github.com>
2022-10-23 13:04:50 +0300

    Add setup_window and setup_title func

--- desktop/windows/base.py
+++ desktop/windows/base.py
@@ -1,10 +1,24 @@
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QMainWindow, QLabel
+from PyQt5.QtCore import QRect, Qt
 
 
 class BaseWindow:
     def __init__(self, client):
         self.client = client
+        self.window = QMainWindow()
 
-    @staticmethod
-    def create_window():
-        return QMainWindow()
+    def setup_window(self, window_title: str):
+        self.window.resize(800, 600)
+        self.window.setWindowTitle(window_title)
+        self.window.setStyleSheet(
+            "background-color: rgb(244, 244, 244);\n" "color: rgb(27, 28, 28);"
+        )
+
+    def setup_title(self, title: str, height: int):
+        self.window.title = QLabel(self.window)
+        self.window.title.setGeometry(QRect(170, height, 450, 50))
+        self.window.title.setAlignment(Qt.AlignCenter)
+        self.window.title.setText(title)
+        self.window.title.setStyleSheet(
+            "\n" "color: rgb(0, 0, 0);\n" 'font: 18pt ".SF NS Text";'
+        )
