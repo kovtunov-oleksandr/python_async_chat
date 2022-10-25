8f1f506a55bda9977ffac50594cc0714c83f0a50
KateSamus <109095866+KateSamus@users.noreply.github.com>
2022-10-25 16:09:16 +0300

    Correct name of SignUpWindowHandler and SignInWindowHandler

--- desktop/windows/auth/__init__.py
+++ desktop/windows/auth/__init__.py
@@ -1,3 +1,3 @@
-from desktop.windows.auth.sign_in_window import SignInWindow
-from desktop.windows.auth.sign_up_window import SignUpWindow
+from desktop.windows.auth.sign_in_window import SignInWindowHandler
+from desktop.windows.auth.sign_up_window import SignUpWindowHandler
 
