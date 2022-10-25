28eac53f8abbde3af19b003a03b98456066c2db8
KateSamus <109095866+KateSamus@users.noreply.github.com>
2022-10-25 16:06:28 +0300

    Change name to BaseWindowHandler Make it abstract class Change name of setup_window() to setup_background() Add abstract method setup_window()

--- desktop/windows/base.py
+++ desktop/windows/base.py
@@ -1,20 +1,21 @@
 from PyQt5.QtWidgets import QMainWindow, QLabel
 from PyQt5.QtCore import QRect, Qt
+import abc
 
 
-class BaseWindow:
+class BaseWindowHandler(abc.ABC):
     def __init__(self, client):
         self.client = client
         self.window = QMainWindow()
 
-    def setup_window(self, window_title: str):
+    def setup_background(self, window_title: str = None):
         self.window.resize(800, 600)
         self.window.setWindowTitle(window_title)
         self.window.setStyleSheet(
             "background-color: rgb(244, 244, 244);\n" "color: rgb(27, 28, 28);"
         )
 
-    def setup_title(self, title: str, height: int):
+    def setup_title(self, title: str = None, height: int = None):
         self.window.title = QLabel(self.window)
         self.window.title.setGeometry(QRect(170, height, 450, 50))
         self.window.title.setAlignment(Qt.AlignCenter)
@@ -22,3 +23,7 @@ class BaseWindow:
         self.window.title.setStyleSheet(
             "\n" "color: rgb(0, 0, 0);\n" 'font: 18pt ".SF NS Text";'
         )
+
+    @abc.abstractmethod
+    def setup_window(self):
+        ...
