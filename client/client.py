e76e6167843e0dec8973fab83a61b6e91396658b
dpirohov <1355579555AsdF>
2022-10-27 12:17:49 +0300

    Reformat sign_in client method

--- client/client.py
+++ client/client.py
@@ -1,8 +1,10 @@
+import json
 from utils.protocol.client import Client
 from utils.protocol.message import Message
 
 
 class ChatClient(Client):
 
-    def sign_in(self, message: Message):
+    def sign_in(self, data: dict):
+        message = Message("sign_in", "_", "server", "_", json.dumps(data))
         self.send_message(message)
