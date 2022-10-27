8f1d868651de474b5d8b0b492594256fa1137ad4
KateSamus <109095866+KateSamus@users.noreply.github.com>
2022-10-27 22:31:37 +0300

    Add sign_up method to ChatClient

--- client/client.py
+++ client/client.py
@@ -1,5 +1,12 @@
+import asyncio
+
 from utils.protocol import Client
+from utils.protocol.message import Message
+import json
 
 
 class ChatClient(Client):
-    ...
+    async def sign_up(self, data: dict):
+        request = Message("sign_up", "client", "server", "_", json.dumps(data))
+        await self.send_message(request)
+
