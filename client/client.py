5f99d05315e8cbb2578f52ec015ceab8ea87ab96
AOYastrebov <AOYastrebov@gmail.com>
2022-10-31 18:42:56 +0200

    Added method create_chat in the client

--- client/client.py
+++ client/client.py
@@ -1,5 +1,9 @@
 from utils.protocol import Client
+from utils.protocol.message import Message
+import json
 
 
 class ChatClient(Client):
-    ...
+    async def create_chat(self, data: dict):
+        message = Message("create_chat", "client", "server", "_", json.dumps(data))
+        await self.send_message(message)
