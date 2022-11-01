baf554f1bb2996c58d96442982b0e7ed3067cf37
Alaru-dev <pobrom@gmail.com>
2022-11-01 22:17:47 +0200

    add answer_handler to utils/protocol/client (one method: sig_in) add to instance:  - nickname  - session_token

--- utils/protocol/client.py
+++ utils/protocol/client.py
@@ -7,24 +7,35 @@ class Client:
     def __init__(self, host, port):
         self.host = host
         self.port = port
+        self.session = []
+        self.nickname = None
         self.connection = None
 
     async def connect(self):
+        """When create Client instance create connection"""
         reader, writer = await asyncio.open_connection(self.host, self.port)
         self.connection = Connection(writer, reader)
 
     async def read_messages(self):
         async for message in self.connection.start_reading():
-            message
+            self.answer_handler(message)
 
     async def send_message(self, message: Message):
-        return self.connection.send_message_wait_answer(message)
+        await self.connection.send_message_wait_answer(message)
 
-    async def quick_start(self):
+    def answer_handler(self, message: Message):
+        if (
+            message.command == "sign_in"
+            and message.content == '{"response": "SUCCESSFULLY LOGGED IN"}'
+        ):
+            self.session = message.token
+        return message
+
+    async def quick_start(self, message):
         await self.connect()
+        await asyncio.create_task(self.send_message(message))
         await asyncio.create_task(self.read_messages())
 
 
 if __name__ == "__main__":
-    client = Client("localhost", 5050)
-    asyncio.run(client.quick_start())
+    pass
