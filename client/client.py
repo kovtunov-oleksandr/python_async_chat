b510adcc6184ce4f28cab39b43dbee752843d228
KateSamus <109095866+KateSamus@users.noreply.github.com>
2022-11-03 20:30:40 +0200

    Shorthand imports

--- client/client.py
+++ client/client.py
@@ -1,5 +1,4 @@
-from utils.protocol import Client
-from utils.protocol.message import Message
+from utils.protocol import Client, Message
 import json
 
 
@@ -7,4 +6,3 @@ class ChatClient(Client):
     async def sign_up(self, data: dict):
         request = Message("sign_up", "client", "server", "_", json.dumps(data))
         await self.send_message(request)
-
