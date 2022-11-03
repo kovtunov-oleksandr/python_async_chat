08a89fe1bc51515a8d124f659db121904734e8b2
KateSamus <109095866+KateSamus@users.noreply.github.com>
2022-11-03 20:36:13 +0200

    - Shorthand imports - Change check_nickname_password_credentials func - Add session argument to handle_response func

--- server/routes/sign_up.py
+++ server/routes/sign_up.py
@@ -1,11 +1,10 @@
-from server.config import server
-from server.models.user import User
-from server.server_utils.db_utils import async_session
-from utils.protocol.message import Message
-from utils.protocol.connection import Connection
-from utils.logger import logger
-from sqlalchemy.future import select
 import json
+from sqlalchemy.future import select
+from server.config import server
+from server.server_utils.db_utils import async_session
+from server.models.user import User
+from utils.protocol import Message, Connection
+from utils.logger import logger
 
 
 @server.message_handler("sign_up")
@@ -14,66 +13,66 @@ async def sign_up(request: Message, connection: Connection):
     logger.info(
         f"Sign up attempt {content.get('nickname'), content.get('password'), content.get('email')}"
     )
-    credentials = check_nickname_password_credentials(content.get("nickname"), content.get("password"))
-    if next(credentials) is not True:
+    nickname_credentials, password_credentials = check_nickname_password_credentials(
+        content.get("nickname"), content.get("password")
+    )
+    if nickname_credentials is not True:
         return await handle_response(
-            request.sender,
             "NICKNAME CANNOT START WITH A SPACE AND MUST CONTAINS ONLY 4 TO 16 CHARACTERS OF THE ENGLISH ALPHABET",
             connection,
         )
-    if next(credentials) is not True:
+    if password_credentials is not True:
         return await handle_response(
-            request.sender,
             "PASSWORD CANNOT START WITH A SPACE AND CONTAINS MORE THAN 50 CHARACTERS",
             connection,
         )
-    async with async_session() as session:
-        is_nickname_in_db = await session.scalar(
+    async with async_session() as session, session.begin():
+        user = await session.scalar(
             select(User).where(User.nickname == content.get("nickname"))
         )
-        if is_nickname_in_db is not None:
+        if user is not None:
             return await handle_response(
-                request.sender, "THIS LOGIN IS NOT UNIQUE", connection
+                "THIS LOGIN IS NOT UNIQUE", connection, session
             )
         else:
             return await handle_response(
-                request.sender, "REGISTRATION IS SUCCESSFUL", connection, content
+                "REGISTRATION IS SUCCESSFUL", connection, session, content
             )
 
 
 def check_nickname_password_credentials(nickname: str, password: str):
-    if (
-            4 <= len(nickname) <= 16
-            and (nickname.isascii() is True and nickname.isalpha() is True)
-            and nickname[0] != " "
-    ):
-        yield True
-    else:
-        yield False
-    if len(password) <= 50 and password.isascii() is True and password[0] != " ":
-        yield True
-    yield False
+    nickname_result = (
+        4 <= len(nickname) <= 16
+        and (nickname.isascii() is True and nickname.isalpha() is True)
+        and nickname[0] != " "
+    ) and True
+    password_result = (
+        len(password) <= 50 and password.isascii() is True and password[0] != " "
+    ) and True
+    return nickname_result, password_result
 
 
-async def add_user_to_db(content: dict, connection: Connection):
+async def add_user_to_db(content: dict, session, connection: Connection):
     user = User(
         nickname=content.get("nickname"),
         password=content.get("password"),
         email=content.get("email"),
     )
-    async with async_session() as session, session.begin():
-        session.add(user)
+    session.add(user)
     logger.info(
         f"New user was registered: <{user.nickname}>, connection: {connection.writer.get_extra_info('peername')}"
     )
 
 
 async def handle_response(
-    sender: str, response_text: str, connection: Connection, content: dict = None
+    response_text: str,
+    connection: Connection,
+    session: async_session = None,
+    content: dict = None,
 ):
     response = Message(
-        "sign_up", "server", sender, "_", json.dumps({"response": response_text})
+        "sign_up", "server", "client", "_", json.dumps({"response": response_text})
     )
     if response_text == "REGISTRATION IS SUCCESSFUL":
-        await add_user_to_db(content, connection)
+        await add_user_to_db(content, session, connection)
     return await connection.send_message(response)
