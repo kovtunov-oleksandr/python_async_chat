7dca8c3432dc958177d67d104c179c391dc007c1
dpirohov <1355579555AsdF>
2022-11-03 22:16:47 +0200

    Optimize imports

---
+++ utils/protocol/__init__.py
@@ -0,0 +1,5 @@
+from utils.protocol.client import Client
+from utils.protocol.connection import Connection
+from utils.protocol.message import Message
+from utils.protocol.server import Server
+from utils.protocol.waiter import Waiter
