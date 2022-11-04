c08a0d48ce4251d2a839ab701da0877ed899619c
KateSamus <109095866+KateSamus@users.noreply.github.com>
2022-11-04 12:00:23 +0200

    Use decode Message methods

--- client/client.py
+++ client/client.py
@@ -1,8 +1,7 @@
 from utils.protocol import Client, Message
-import json
 
 
 class ChatClient(Client):
     async def sign_up(self, data: dict):
-        request = Message("sign_up", "client", "server", "_", json.dumps(data))
-        await self.send_message(request)
+        message = Message("sign_up", "client", "server", "_", Message.decode_content_to_json(data))
+        await self.send_message(message)
