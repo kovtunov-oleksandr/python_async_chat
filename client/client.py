f397eba1a0843fb965c785f9d51619096efe6211
AOYastrebov <AOYastrebov@gmail.com>
2022-11-05 10:49:32 +0200

    added def create_chat to client

--- client/client.py
+++ client/client.py
@@ -5,5 +5,5 @@ import json
 
 class ChatClient(Client):
     async def create_chat(self, data: dict):
-        message = Message("create_chat", "client", "server", "_", json.dumps(data))
+        message = Message("create_chat", "client", "server", "_", Message.decode_content_to_json(data))
         await self.send_message(message)
