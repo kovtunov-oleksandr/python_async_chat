97bbcd5c6ff37da9680bcb38c7df08df5b9ea992
dpirohov <1355579555AsdF>
2022-11-05 12:17:25 +0200

    Fix protocol message from client to server

--- client/client.py
+++ client/client.py
@@ -6,5 +6,5 @@ from utils.protocol.message import Message
 class ChatClient(Client):
 
     async def sign_in(self, data: dict):
-        message = Message("sign_in", "_", "server", "_", Message.decode_content_to_json(data))
+        message = Message("sign_in", "client", "server", "_", Message.decode_content_to_json(data))
         await self.send_message(message)
