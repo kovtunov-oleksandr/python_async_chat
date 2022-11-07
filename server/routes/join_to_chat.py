9b4db7f1b63b34e47705372227ebfec6a0692686
KateSamus <109095866+KateSamus@users.noreply.github.com>
2022-11-07 16:11:50 +0200

    Replace chat_name with chat_id Change failed message Check if user is in chat already

--- server/routes/join_to_chat.py
+++ server/routes/join_to_chat.py
@@ -9,12 +9,12 @@ from utils.logger import logger
 @server.message_handler("join_to_chat")
 async def join_to_chat(message: Message, connection: Connection):
     content = message.encode_content_from_json()
-    chat_name, user_id, permission = map(
-        lambda x: content.get(x), ("chat_name", "user_id", "permission")
+    chat_id, user_id, permission = map(
+        lambda x: content.get(x), ("chat_id", "user_id", "permission")
     )
-    logger.info(f"Attempt to join to chat from {user_id}, {chat_name}")
+    logger.info(f"Attempt to join to chat from {user_id}, {chat_id}")
     async with async_session() as session, session.begin():
-        chat = await session.scalar(select(Chat).where(Chat.chat_name == chat_name))
+        chat = await session.scalar(select(Chat).where(Chat.chat_id == chat_id))
         if chat is None:
             response = Message(
                 "join_to_chat",
@@ -26,25 +26,37 @@ async def join_to_chat(message: Message, connection: Connection):
                 ),
             )
         else:
-            chat_member = ChatMember(
-                user_id=user_id, chat_id=chat.id, permissions=permission
-            )
-            session.add(chat_member)
-            response = Message(
-                "join_to_chat",
-                "server",
-                "client",
-                "_",
-                Message.decode_content_to_json(
-                    {
-                        "response": "YOU WAS JOINED TO CHAT SUCCESSFULLY",
-                        "chat_id": chat.id,
-                        "chat_name": chat_name,
-                    }
-                ),
-            )
-        logger.info(
-            f"User was added to chat: <{user_id}> - <{chat_name}>, "
-            + f"connection: {connection.writer.get_extra_info('peername')}"
-        )
+            result = await session.scalar(select(ChatMember).where(ChatMember.user_id == user_id))
+            if result is not None:
+                response = Message(
+                    "join_to_chat",
+                    "server",
+                    "client",
+                    "_",
+                    Message.decode_content_to_json(
+                        {"response": "YOU ARE ALREADY IN THIS CHAT"}
+                    ),
+                )
+            else:
+                chat_member = ChatMember(
+                    user_id=user_id, chat_id=chat_id, permissions=permission
+                )
+                session.add(chat_member)
+                response = Message(
+                    "join_to_chat",
+                    "server",
+                    "client",
+                    "_",
+                    Message.decode_content_to_json(
+                        {
+                            "response": "YOU JOINED THE CHAT",
+                            "chat_id": chat.id,
+                            "chat_name": chat.name,
+                        }
+                    ),
+                )
+                logger.info(
+                    f"User was added to chat: <{user_id}> - <{chat_id}>, "
+                    + f"connection: {connection.writer.get_extra_info('peername')}"
+                )
     return await connection.send_message(response)
