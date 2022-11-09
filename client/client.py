eedd06a6ef3ee009ea206df5aa374eb9126bd998
AOYastrebov <AOYastrebov@gmail.com>
2022-11-09 22:53:59 +0200

    created func get_all_groups

--- client/client.py
+++ client/client.py
@@ -18,3 +18,7 @@ class ChatClient(Client):
     async def get_group_members(self, data: dict):
         message = Message("get_group_members", "client", "server", self.token, Message.decode_content_to_json(data))
         await self.send_message(message)
+
+    async def get_all_groups(self, data: dict):
+        message = Message("get_all_groups", "client", "server", self.token, Message.decode_content_to_json(data))
+        await self.send_message(message)
---
+++ server/routes/get_all_groups.py
@@ -0,0 +1,32 @@
+from sqlalchemy.future import select
+from server.models import Chat
+from server.server_utils.db_utils import async_session
+from server.config import server
+from utils.logger import logger
+from utils.protocol import Message, Connection
+
+
+@server.message_handler("get_all_groups")
+async def get_group_members(message: Message, connection: Connection):
+    logger.info(f"Attempt to get all groups")
+    async with async_session() as session, session.begin():
+        chats = await session.scalars(select(Chat).where(Chat.type == 1))
+        if chats.first() is None:
+            response = Message(
+                "get_all_groups",
+                "server",
+                "client",
+                "_",
+                Message.decode_content_to_json({"response": f"THERE IS NO CHAT"}),
+            )
+        else:
+            chats = await session.scalars(select(Chat).where(Chat.type == 1))
+            chats = [{"chat_name": chat.chat_name, "id": chat.id} for chat in chats]
+            response = Message(
+                "create_chat",
+                "server",
+                "client",
+                "_",
+                Message.decode_content_to_json({"response": "LIST OF CHATS RECEIVED", "chats": chats}),
+            )
+        return await connection.send_message(response)
