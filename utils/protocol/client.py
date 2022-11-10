85fc7f6c1b61945aaae813801cc6398bc8ff9d81
Alaru-dev <pobrom@gmail.com>
2022-11-10 18:57:14 +0200

    updated base client.py by:  added to instance token, nickname, user_id  added sign_in_answer_handler method which add to instance token, nickname, user_id

--- utils/protocol/client.py
+++ utils/protocol/client.py
@@ -1,41 +1,38 @@
 import asyncio
-from utils.protocol.connection import Connection
-from utils.protocol.message import Message
+from utils.protocol import Connection, Message
 
 
 class Client:
     def __init__(self, host, port):
         self.host = host
         self.port = port
-        self.session = []
         self.nickname = None
+        self.token = None
+        self.user_id = None
         self.connection = None
 
     async def connect(self):
-        """When create Client instance create connection"""
         reader, writer = await asyncio.open_connection(self.host, self.port)
         self.connection = Connection(writer, reader)
 
     async def read_messages(self):
         async for message in self.connection.start_reading():
-            self.answer_handler(message)
+            self.sign_in_answer_handler(message)
 
     async def send_message(self, message: Message):
         await self.connection.send_message_wait_answer(message)
 
-    def answer_handler(self, message: Message):
-        if (
-            message.command == "sign_in"
-            and message.content == '{"response": "SUCCESSFULLY LOGGED IN"}'
-        ):
-            self.session = message.token
-        return message
-
-    async def quick_start(self, message):
+    async def quick_start(self):
         await self.connect()
-        await asyncio.create_task(self.send_message(message))
         await asyncio.create_task(self.read_messages())
 
-
-if __name__ == "__main__":
-    pass
+    def sign_in_answer_handler(self, message: Message):
+        content = message.encode_content_from_json()
+        if (
+                message.command == "sign_in"
+                and content["response"] == "SUCCESSFULLY LOGGED IN"
+        ):
+            self.token = message.token
+            self.nickname = content["nickname"]
+            self.user_id = content["user_id"]
+        return message
