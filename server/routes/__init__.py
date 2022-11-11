fb4dd734694880635ede5dc3df719b16834b282d
Alaru-dev <pobrom@gmail.com>
2022-11-11 13:33:08 +0200

    added method leave_chat to client/client added module leave_chat to server/routes updated init at server/routes

--- server/routes/__init__.py
+++ server/routes/__init__.py
@@ -1,1 +1,7 @@
-from server.routes import sign_in, sign_up, create_chat, get_group_members
+from server.routes import (
+    sign_in,
+    sign_up,
+    create_chat,
+    get_group_members,
+    leave_chat,
+)
