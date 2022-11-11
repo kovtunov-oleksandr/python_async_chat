6870e553539e50e33d5bdf6446967961f7b5b97e
AOYastrebov <AOYastrebov@gmail.com>
2022-11-11 20:00:27 +0200

    fixed some bugs before if 17 row. return await move to the left

--- server/routes/get_all_groups.py
+++ server/routes/get_all_groups.py
@@ -13,8 +13,8 @@ async def get_group_members(message: Message, connection: Connection):
     logger.info(f"Attempt getting all groups by user_id: {user_id}")
     async with async_session() as session:
         chats = await session.scalars(select(Chat).where(Chat.type == 1))
-        chats_is_none = await session.scalars(select(Chat).where(Chat.type == 1))
-        if chats_is_none.first() is None:
+        chats = [{"chat_name": chat.chat_name, "chat_id": chat.id} for chat in chats]
+        if not chats:
             response = Message(
                 "get_all_groups",
                 "server",
@@ -23,7 +23,6 @@ async def get_group_members(message: Message, connection: Connection):
                 Message.decode_content_to_json({"response": f"THERE IS NO CHAT"}),
             )
         else:
-            chats = [{"chat_name": chat.chat_name, "chat_id": chat.id} for chat in chats]
             response = Message(
                 "get_all_groups",
                 "server",
@@ -31,4 +30,4 @@ async def get_group_members(message: Message, connection: Connection):
                 "_",
                 Message.decode_content_to_json({"response": "LIST OF CHATS RECEIVED", "chats": chats}),
             )
-        return await connection.send_message(response)
+    return await connection.send_message(response)
