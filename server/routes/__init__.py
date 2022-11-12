43fab7053d680eca08a255c4638fa24da5888d94
Alaru-dev <pobrom@gmail.com>
2022-11-12 22:02:37 +0200

    updated send_message_to_chat: - used one request to db - changed if-else cases

--- server/routes/__init__.py
+++ server/routes/__init__.py
@@ -1,1 +1,9 @@
-from server.routes import sign_in, sign_up, create_chat, get_group_members, get_all_groups, join_to_group_chat
+from server.routes import (
+    sign_in,
+    sign_up,
+    create_chat,
+    get_group_members,
+    get_all_groups,
+    join_to_group_chat,
+    send_message_to_chat,
+)
--- server/routes/send_message_to_chat.py
+++ server/routes/send_message_to_chat.py
@@ -14,51 +14,31 @@ async def send_message_to_chat(message: Message, connection: Connection):
         f"Attempt send_message_to_chat from user_id {user_id}, to chat_id {chat_id}"
     )
     async with async_session() as session:
-        chat = await session.scalar(select(Chat).where(Chat.id == chat_id))
-        if chat is None:
-            response = Message(
-                "send_message_to_chat",
-                "server",
-                "client",
-                "_",
-                Message.decode_content_to_json(
-                    {"response": "THIS CHAT DOES NOT EXIST"}
-                ),
-            )
-        else:
-            member_in_chat = await session.scalar(
-                select(ChatMember).where(
-                    ChatMember.user_id == user_id, ChatMember.chat_id == chat_id
-                )
-            )
-            if member_in_chat is not None:
-                chat_members_id = await session.scalars(
-                    select(ChatMember.user_id).where(ChatMember.chat_id == chat_id)
-                )
-                users_id = chat_members_id.all()
+        chat_members_id = await session.scalars(
+            select(ChatMember.user_id).where(ChatMember.chat_id == chat_id)
+        )
+        users_id = chat_members_id.all()
+        if len(users_id) != 0:
+            if user_id in users_id:
+                users_id.remove(user_id)
                 message_to_chat = Message(
                     message.command, "server", "client", "_", message.content
                 )
                 for user in users_id:
-                    if user != user_id and user in server.sessions:
+                    if user in server.sessions:
                         await server.sessions[user]["user_connection"].send_message(
                             message_to_chat
                         )
-                response = Message(
-                    "Send_message_to_chat",
-                    "server",
-                    "client",
-                    "_",
-                    Message.decode_content_to_json({"response": "MESSAGE SENT"}),
-                )
+                response_content = {"response": "MESSAGE SENT"}
             else:
-                response = Message(
-                    "send_message_to_chat",
-                    "server",
-                    "client",
-                    "_",
-                    Message.decode_content_to_json(
-                        {"response": "YOU ARE NOT IN THIS CHAT"}
-                    ),
-                )
+                response_content = {"response": "YOU ARE NOT IN THIS CHAT"}
+        else:
+            response_content = {"response": "THIS CHAT DOES NOT EXIST"}
+        response = Message(
+            "Send_message_to_chat",
+            "server",
+            "client",
+            "_",
+            Message.decode_content_to_json(response_content),
+        )
         return await connection.send_message(response)
