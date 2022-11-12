ad255be24f48a0fee22a8800a6bb1d088f323a21
Alaru-dev <pobrom@gmail.com>
2022-11-12 23:48:47 +0200

    updated client/client

--- client/client.py
+++ client/client.py
@@ -74,3 +74,14 @@ class ChatClient(Client):
             Message.decode_content_to_json(data),
         )
         return await self.send_message(message)
+
+    async def send_message_to_chat(self, data: dict):
+        data["user_id"], data["nickname"] = self.user_id, self.nickname
+        message = Message(
+            SendMessageToChat.COMMAND.value,
+            Protocol.CLIENT.value,
+            Protocol.SERVER.value,
+            self.token,
+            Message.decode_content_to_json(data),
+        )
+        return await self.send_message(message)
