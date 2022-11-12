33c60ca03f02f6c589d21c349ff90c84ee26eccd
dpirohov <1355579555AsdF>
2022-11-12 22:35:46 +0200

    Update user roles and chat types

--- global_enums/create_chat.py
+++ global_enums/create_chat.py
@@ -6,3 +6,9 @@ class CreateChat(enum.Enum):
     COMMAND = "create_chat"
     RESPONSE_CHAT_NAME_EXISTS = "CHAT NAME IS NOT UNIQUE"
     RESPONSE_CHAT_CREATED = "YOU CREATED CHAT"
+
+    USER = 0
+    ADMIN = 1
+
+    PRIVATE = 0
+    PUBLIC = 1
