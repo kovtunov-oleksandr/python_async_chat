c8b9c85c398aa98cbbee3688b1b2b5a025e39be9
KateSamus <109095866+KateSamus@users.noreply.github.com>
2022-11-14 19:56:39 +0200

    Cut down code Reformat code with black and flake8

--- server/routes/create_chat.py
+++ server/routes/create_chat.py
@@ -20,72 +20,61 @@ async def create_chat(message: Message, connection: Connection):
     async with async_session() as session, session.begin():
         chat = await session.scalar(select(Chat).where(Chat.chat_name == chat_name))
         if chat is not None:
-            response = Message(
-                CreateChat.COMMAND.value,
-                Protocol.SERVER.value,
-                Protocol.CLIENT.value,
-                Protocol.EMPTY_TOKEN.value,
-                Message.decode_content_to_json(
-                    {"response": CreateChat.RESPONSE_CHAT_NAME_EXISTS.value}
-                ),
+            content = Message.decode_content_to_json(
+                {"response": CreateChat.RESPONSE_CHAT_NAME_EXISTS.value}
             )
         else:
             if chat_type == 0:
-                response = await asyncio.create_task(second_user_verification(second_user_id, session))
-            if chat_type == 1 or response is True:
+                content = await asyncio.create_task(
+                    second_user_verification(second_user_id, session)
+                )
+            if chat_type == 1 or content is True:
                 chat = Chat(chat_name=chat_name, creator_id=creator_id, type=chat_type)
                 session.add(chat)
                 await session.flush()
                 creator_chat_member = ChatMember(
-                    user_id=creator_id, chat_id=chat.id, permissions=CreateChat.ADMIN.value
+                    user_id=creator_id,
+                    chat_id=chat.id,
+                    permissions=CreateChat.ADMIN.value,
                 )
                 session.add(creator_chat_member)
                 if chat_type == 0:
                     second_chat_member = ChatMember(
-                        user_id=second_user_id, chat_id=chat.id, permissions=CreateChat.ADMIN.value,
+                        user_id=second_user_id,
+                        chat_id=chat.id,
+                        permissions=CreateChat.ADMIN.value,
                     )
                     session.add(second_chat_member)
-                response = Message(
-                    CreateChat.COMMAND.value,
-                    Protocol.SERVER.value,
-                    Protocol.CLIENT.value,
-                    Protocol.EMPTY_TOKEN.value,
-                    Message.decode_content_to_json(
-                        {
-                            "response": CreateChat.RESPONSE_CHAT_CREATED.value,
-                            "chat_id": chat.id,
-                            "chat_name": chat_name,
-                        }
-                    ),
+                content = Message.decode_content_to_json(
+                    {
+                        "response": CreateChat.RESPONSE_CHAT_CREATED.value,
+                        "chat_id": chat.id,
+                        "chat_name": chat_name,
+                    }
                 )
+    response = Message(
+        CreateChat.COMMAND.value,
+        Protocol.SERVER.value,
+        Protocol.CLIENT.value,
+        Protocol.EMPTY_TOKEN.value,
+        content,
+    )
     return await connection.send_message(response)
 
 
 async def second_user_verification(second_user_id: int, session: async_session):
     if second_user_id is None:
-        response = Message(
-            CreateChat.COMMAND.value,
-            Protocol.SERVER.value,
-            Protocol.CLIENT.value,
-            Protocol.EMPTY_TOKEN.value,
-            Message.decode_content_to_json(
-                {"response": CreateChat.RESPONSE_SECOND_USER_SPECIFIED.value}
-            ),
+        content = Message.decode_content_to_json(
+            {"response": CreateChat.RESPONSE_SECOND_USER_SPECIFIED.value}
         )
     else:
         second_user = await session.scalar(
             select(User).where(User.id == second_user_id)
         )
         if second_user is not None:
-            response = True
+            content = True
         else:
-            response = Message(
-                CreateChat.COMMAND.value,
-                Protocol.SERVER.value,
-                Protocol.CLIENT.value,
-                Protocol.EMPTY_TOKEN.value,
-                Message.decode_content_to_json(
-                    {"response": CreateChat.RESPONSE_SECOND_USER_NOT_FOUND.value}
-                ),
+            content = Message.decode_content_to_json(
+                {"response": CreateChat.RESPONSE_SECOND_USER_NOT_FOUND.value}
             )
-    return response
+    return content
