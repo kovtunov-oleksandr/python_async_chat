5c17768a686f1f3f54a015c3e988d999831cdfb0
KateSamus <109095866+KateSamus@users.noreply.github.com>
2022-11-14 20:26:22 +0200

    Change content to response_content

--- server/routes/create_chat.py
+++ server/routes/create_chat.py
@@ -20,15 +20,13 @@ async def create_chat(message: Message, connection: Connection):
     async with async_session() as session, session.begin():
         chat = await session.scalar(select(Chat).where(Chat.chat_name == chat_name))
         if chat is not None:
-            content = Message.decode_content_to_json(
-                {"response": CreateChat.RESPONSE_CHAT_NAME_EXISTS.value}
-            )
+            response_content = {"response": CreateChat.RESPONSE_CHAT_NAME_EXISTS.value}
         else:
             if chat_type == 0:
-                content = await asyncio.create_task(
+                response_content = await asyncio.create_task(
                     second_user_verification(second_user_id, session)
                 )
-            if chat_type == 1 or content is True:
+            if chat_type == 1 or response_content is True:
                 chat = Chat(chat_name=chat_name, creator_id=creator_id, type=chat_type)
                 session.add(chat)
                 await session.flush()
@@ -45,36 +43,30 @@ async def create_chat(message: Message, connection: Connection):
                         permissions=CreateChat.ADMIN.value,
                     )
                     session.add(second_chat_member)
-                content = Message.decode_content_to_json(
-                    {
+                response_content = {
                         "response": CreateChat.RESPONSE_CHAT_CREATED.value,
                         "chat_id": chat.id,
                         "chat_name": chat_name,
-                    }
-                )
+                }
     response = Message(
         CreateChat.COMMAND.value,
         Protocol.SERVER.value,
         Protocol.CLIENT.value,
         Protocol.EMPTY_TOKEN.value,
-        content,
+        Message.decode_content_to_json(response_content)
     )
     return await connection.send_message(response)
 
 
 async def second_user_verification(second_user_id: int, session: async_session):
     if second_user_id is None:
-        content = Message.decode_content_to_json(
-            {"response": CreateChat.RESPONSE_SECOND_USER_SPECIFIED.value}
-        )
+        response_content = {"response": CreateChat.RESPONSE_SECOND_USER_SPECIFIED.value}
     else:
         second_user = await session.scalar(
             select(User).where(User.id == second_user_id)
         )
         if second_user is not None:
-            content = True
+            response_content = True
         else:
-            content = Message.decode_content_to_json(
-                {"response": CreateChat.RESPONSE_SECOND_USER_NOT_FOUND.value}
-            )
-    return content
+            response_content = {"response": CreateChat.RESPONSE_SECOND_USER_NOT_FOUND.value}
+    return response_content
