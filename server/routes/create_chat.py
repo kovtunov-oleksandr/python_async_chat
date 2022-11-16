647e5f0dfa126b4bb5872ab6ef415f8e0d0ed306
KateSamus <109095866+KateSamus@users.noreply.github.com>
2022-11-16 14:51:28 +0200

    Cut off create_chat func Add general add_chat_and_creator_to_db func to private and public chat Combine two second-user-error enams into one

--- server/routes/create_chat.py
+++ server/routes/create_chat.py
@@ -1,5 +1,3 @@
-import asyncio
-
 from sqlalchemy.future import select
 from server.models import Chat, ChatMember, User
 from server.server_utils.db_utils import async_session
@@ -21,52 +19,48 @@ async def create_chat(message: Message, connection: Connection):
         chat = await session.scalar(select(Chat).where(Chat.chat_name == chat_name))
         if chat is not None:
             response_content = {"response": CreateChat.RESPONSE_CHAT_NAME_EXISTS.value}
-        else:
-            if chat_type == 0:
-                response_content = await asyncio.create_task(
-                    second_user_verification(second_user_id, session)
-                )
-            if chat_type == 1 or response_content is True:
-                chat = Chat(chat_name=chat_name, creator_id=creator_id, type=chat_type)
-                session.add(chat)
-                await session.flush()
-                creator_chat_member = ChatMember(
-                    user_id=creator_id,
-                    chat_id=chat.id,
-                    permissions=CreateChat.ADMIN.value,
-                )
-                session.add(creator_chat_member)
-                if chat_type == 0:
-                    second_chat_member = ChatMember(
-                        user_id=second_user_id,
-                        chat_id=chat.id,
-                        permissions=CreateChat.ADMIN.value,
-                    )
-                    session.add(second_chat_member)
-                response_content = {
-                        "response": CreateChat.RESPONSE_CHAT_CREATED.value,
-                        "chat_id": chat.id,
-                        "chat_name": chat_name,
-                }
+            return await send_message(connection, response_content)
+        if chat_type == CreateChat.PRIVATE.value:
+            second_user = await session.scalar(select(User).where(User.id == second_user_id))
+            if second_user is None:
+                response_content = {"response": CreateChat.RESPONSE_SECOND_USER_NOT_FOUND.value}
+                return await send_message(connection, response_content)
+            chat_id, response_content = await add_chat_and_creator_to_db(session, creator_id, chat_name, chat_type)
+            second_chat_member = ChatMember(
+                user_id=second_user_id,
+                chat_id=chat_id,
+                permissions=CreateChat.ADMIN.value,
+            )
+            session.add(second_chat_member)
+            return await send_message(connection, response_content)
+        chat_id, response_content = await add_chat_and_creator_to_db(session, creator_id, chat_name, chat_type)
+        return await send_message(connection, response_content)
+
+
+async def send_message(connection: Connection, response: dict):
     response = Message(
         CreateChat.COMMAND.value,
         Protocol.SERVER.value,
         Protocol.CLIENT.value,
         Protocol.EMPTY_TOKEN.value,
-        Message.decode_content_to_json(response_content)
+        Message.decode_content_to_json(response)
     )
     return await connection.send_message(response)
 
 
-async def second_user_verification(second_user_id: int, session: async_session):
-    if second_user_id is None:
-        response_content = {"response": CreateChat.RESPONSE_SECOND_USER_SPECIFIED.value}
-    else:
-        second_user = await session.scalar(
-            select(User).where(User.id == second_user_id)
-        )
-        if second_user is not None:
-            response_content = True
-        else:
-            response_content = {"response": CreateChat.RESPONSE_SECOND_USER_NOT_FOUND.value}
-    return response_content
+async def add_chat_and_creator_to_db(session, creator_id, chat_name, chat_type):
+    chat = Chat(chat_name=chat_name, creator_id=creator_id, type=chat_type)
+    session.add(chat)
+    await session.flush()
+    creator_chat_member = ChatMember(
+        user_id=creator_id,
+        chat_id=chat.id,
+        permissions=CreateChat.ADMIN.value,
+    )
+    session.add(creator_chat_member)
+    response_content = {
+        "response": CreateChat.RESPONSE_CHAT_CREATED.value,
+        "chat_id": chat.id,
+        "chat_name": chat_name,
+    }
+    return chat.id, response_content
