0fcd0c9019726862fa6c930ec19ae1f3b3a7157f
KateSamus <109095866+KateSamus@users.noreply.github.com>
2022-11-18 22:09:12 +0200

    Add join_to_group_chat tests Create conftest.py for test_join_to_group_chat

---
+++ tests/test_join_to_group_chat/conftest.py
@@ -0,0 +1,24 @@
+import pytest
+import pytest_asyncio
+from server.server_utils.db_utils import async_session
+from server.models import ChatMember
+from global_enums import CreateChat
+
+
+@pytest.fixture
+def get_amount():
+    return 2
+
+
+@pytest_asyncio.fixture
+async def generate_chat_member(generate_user_in_db, generate_chat_in_db):
+    user = generate_user_in_db[0]
+    chat = generate_chat_in_db
+    async with async_session() as user_session, user_session.begin():
+        chat_member = ChatMember(
+            user_id=user.id, chat_id=chat.id, permissions=CreateChat.USER.value
+        )
+        user_session.add(chat_member)
+    yield chat_member
+    async with async_session() as user_session, user_session.begin():
+        await user_session.delete(chat)
---
+++ tests/test_join_to_group_chat/test_join_to_group_chat.py
@@ -0,0 +1,51 @@
+from sqlalchemy.future import select
+import pytest
+from server.models import ChatMember
+from global_enums import JoinGroup, Protocol
+from utils.protocol.message import Message
+
+
+@pytest.mark.asyncio
+class TestJoinToGroupChat:
+    async def test_successful_join(
+        self, create_user_session, generate_chat_in_db, session
+    ):
+        client = create_user_session
+        chat = generate_chat_in_db
+        response, content = await self.send_message(client, chat.id)
+        assert response.command == JoinGroup.COMMAND.value
+        assert response.sender == Protocol.SERVER.value
+        assert response.receiver == Protocol.CLIENT.value
+        assert content.get("response") == JoinGroup.RESPONSE_CHAT_JOINED.value
+        chat_member = await session.scalar(
+            select(ChatMember).where(
+                ChatMember.chat_id == chat.id, ChatMember.user_id == client.user_id
+            )
+        )
+        assert chat_member
+
+    async def test_failed_join_chat_not_found(self, create_user_session):
+        client = create_user_session
+        chat_id = -1
+        response, content = await self.send_message(client, chat_id)
+        assert response.command == JoinGroup.COMMAND.value
+        assert response.sender == Protocol.SERVER.value
+        assert response.receiver == Protocol.CLIENT.value
+        assert content.get("response") == JoinGroup.RESPONSE_CHAT_NOT_FOUND.value
+
+    async def test_failed_join_user_already_in_chat(
+        self, create_user_session, generate_chat_in_db, generate_chat_member
+    ):
+        client = create_user_session
+        chat = generate_chat_in_db
+        response, content = await self.send_message(client, chat.id)
+        assert response.command == JoinGroup.COMMAND.value
+        assert response.sender == Protocol.SERVER.value
+        assert response.receiver == Protocol.CLIENT.value
+        assert content.get("response") == JoinGroup.RESPONSE_ALREADY_IN_CHAT.value
+
+    async def send_message(self, client, chat_id: int) -> tuple:
+        data = {"chat_id": chat_id, "user_id": client.user_id}
+        response: Message = await client.join_to_group_chat(data)
+        content = response.encode_content_from_json()
+        return response, content
