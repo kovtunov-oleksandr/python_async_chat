2c038009d053b1aa307a18769114b4086eafe829
KateSamus <109095866+KateSamus@users.noreply.github.com>
2022-11-20 20:08:04 +0200

    Add get_chat_name fixture

--- tests/test_create_chat/conftest.py
+++ tests/test_create_chat/conftest.py
@@ -1,6 +1,17 @@
 import pytest
-
+import pytest_asyncio
+from tests.utils.generate_chat_name import generate_chat_name
+from sqlalchemy import delete
+from server.models import Chat
 
 @pytest.fixture
 def get_amount():
     return 2
+
+@pytest_asyncio.fixture
+async def get_chat_name(session):
+    chat_name = generate_chat_name()
+    yield chat_name
+    query = delete(Chat).where(Chat.chat_name == chat_name)
+    await session.execute(query)
+
