b59e76d777d8c72d1692ac66650ab61f8201348d
KateSamus <109095866+KateSamus@users.noreply.github.com>
2022-11-20 20:28:31 +0200

    Create generate_non_exist_user_id fixture

--- tests/test_create_chat/conftest.py
+++ tests/test_create_chat/conftest.py
@@ -1,3 +1,4 @@
+import random
 import pytest
 import pytest_asyncio
 from tests.utils.generate_chat_name import generate_chat_name
@@ -7,6 +8,11 @@ from server.models import Chat
 @pytest.fixture
 def get_amount():
     return 2
+
+@pytest.fixture
+def generate_non_exist_user_id():
+    num = random.randint(9999, 99999)
+    return num
 
 @pytest_asyncio.fixture
 async def get_chat_name(session):
