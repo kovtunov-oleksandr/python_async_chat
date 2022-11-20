226427cd8d8a60fc0b9e64c6cf37a71a980a9032
KateSamus <109095866+KateSamus@users.noreply.github.com>
2022-11-20 20:28:20 +0200

    Create generate_non_exist_chat_id fixture

--- tests/test_join_to_group_chat/conftest.py
+++ tests/test_join_to_group_chat/conftest.py
@@ -1,3 +1,4 @@
+import random
 import pytest
 import pytest_asyncio
 from server.server_utils.db_utils import async_session
@@ -9,6 +10,10 @@ from global_enums import CreateChat
 def get_amount():
     return 2
 
+@pytest.fixture
+def generate_non_exist_chat_id():
+    num = random.randint(9999, 99999)
+    return num
 
 @pytest_asyncio.fixture
 async def generate_chat_member(generate_user_in_db, generate_chat_in_db):
