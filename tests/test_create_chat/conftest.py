ac3095163199a401a0b6bfa8bb82ca15d0419447
KateSamus <109095866+KateSamus@users.noreply.github.com>
2022-11-21 11:51:05 +0200

    Replace generate_chat_name with generate_single_valid_chat_name

--- tests/test_create_chat/conftest.py
+++ tests/test_create_chat/conftest.py
@@ -1,7 +1,7 @@
 import random
 import pytest
 import pytest_asyncio
-from tests.utils.generate_chat_name import generate_chat_name
+from tests.utils.generate_chats import generate_single_valid_chat_name
 from sqlalchemy import delete
 from server.models import Chat
 
@@ -19,7 +19,7 @@ def generate_non_exist_user_id():
 
 @pytest_asyncio.fixture
 async def get_chat_name(session):
-    chat_name = generate_chat_name()
+    chat_name = generate_single_valid_chat_name()
     yield chat_name
     query = delete(Chat).where(Chat.chat_name == chat_name)
     await session.execute(query)
